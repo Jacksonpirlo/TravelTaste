941e61c42551ca92e70da89250de68f89ca3489e
Jacksonpirlo <jacksonflorezp@gmail.com>
2024-12-27 10:07:50 -0500

    Subiendo primera prueba de TravelTaste

---
+++ src/App.vue
@@ -0,0 +1,110 @@
+<template>
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="./img/carousel-theme1.jpg" class="d-block w-100" alt="...">
+
+        <div class="carousel-caption d-none d-md-block">
+        <h5>Travel with</h5>
+        <p>a trusted friend, travel with TravelTaste</p>
+      </div>
+      </div>
+      <div class="carousel-item">
+        <img src="./img/carousel-theme2.jpg" class="d-block w-100" alt="...">
+
+        <div class="carousel-caption d-none d-md-block">
+        <h5>Enjoy</h5>
+        <p>And let TravelTaste find your perfect spot</p>
+      </div>
+      </div>
+      <div class="carousel-item">
+        <img src="./img/carousel-theme3.jpg" class="d-block w-100" alt="...">
+
+        <div class="carousel-caption d-none d-md-block">
+        <h5>Prices for</h5>
+        <p>Hotels, restaurants, tours and more here at TravelTaste!</p>
+      </div>
+      </div>
+
+      <div class="carousel-item">
+        <img src="./img/carousel-theme4.jpg" class="d-block w-100" alt="...">
+
+        <div class="carousel-caption d-none d-md-block">
+        <h5>Discover</h5>
+        <p>and enjoy new cuisines and cultures from around the world</p>
+      </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+
+    <div class="container-main">
+      <div><p>Search by your tastes </p></div>
+      <span>  </span>
+      <div><p>Touristic places </p></div>
+    </div>
+
+    <AppFooter />
+</template>
+
+<script>
+
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  name: 'App',
+  components: {
+    AppFooter
+  }
+}
+
+</script>
+
+<style>
+ * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #0f0f0f;
+}
+
+.carousel-item img {
+  height: 70vh;
+  filter: brightness(0.3);
+  object-fit: cover;
+}
+
+.container-main {
+  display: flex;
+  justify-content: space-evenly;
+  color: white;
+  margin: 20px;
+  font-size: 1rem;
+  text-align: center;
+  margin-top: 30px;
+  margin-bottom: 50px;
+  font-family: "Ysabeau SC", serif;
+  font-optical-sizing: auto;
+  text-decoration: underline;
+  font-weight: weight;
+  font-style: normal;
+}
+
+.container-main span {
+background-color: #3d3d3d;
+height: 60px;
+width: 2px;
+margin-left: 20px;
+margin-right: 20px;
+}
+</style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,86 @@
+<template>
+<footer>
+    <div class="footer-desktop">
+        <div class="footer-left">
+        <p>Made with love by</p>
+        <p><a href="#">My github</a></p>
+    </div>
+    <div><p>All rights reserved</p></div>
+    </div>
+
+    <div class="footer-mobile">
+        <div class="footer-left-mobile">
+            <p>Made with love by <a href="#">My github</a></p>
+        </div>
+        <div><p>All rights reserved</p></div>
+    </div>
+</footer>
+</template>
+
+<script>
+export default {
+    name: 'AppFooter',
+}
+</script>
+
+<style scoped>
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+html {
+    font-size: 62.5%;
+}
+
+.footer-desktop {
+    display: none;
+    justify-content: space-evenly;
+    background-color: #000000;
+    align-items: center;
+    height: 10vh;
+    color: white;
+    font-size: .7rem;
+    font-weight: bold;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    text-align: center;
+}
+
+.footer-mobile {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: #000000;
+    align-items: center;
+    height: 10vh;
+    color: white;
+    font-size: .7rem;
+    font-weight: bold;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    text-align: center;
+}
+
+footer .footer-left {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+
+.footer-left p {
+    margin: 5px;
+}
+
+@media screen and (min-width: 768px) {
+    .footer-desktop {
+        display: flex;
+    }
+
+    .footer-mobile {
+        display: none;
+    }
+}
+</style>
