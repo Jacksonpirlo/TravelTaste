e765a736cc55bdbde234fd8b11f759893d34b062
Jacksonpirlo <jacksonflorezp@gmail.com>
2024-12-28 11:20:21 -0500

    Creando la seccion del menu

--- src/components/CardsSelected.vue
+++ src/components/CardsSelected.vue
@@ -1,33 +1,50 @@
 <template>
 <div class="card-container">
 
-        <div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
+        <div class="card" style="width: 15rem;">
+    <img src="../img/carousel-theme1.jpg" class="card-img-top" alt="...">
     <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
+</div>
 
-    <div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
+<div class="card" style="width: 15rem;">
+    <img src="../img/carousel-theme1.jpg" class="card-img-top" alt="...">
     <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
-    </div>
+</div>
 
-
-    <div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
+<div class="card" style="width: 15rem;">
+    <img src="../img/carousel-theme1.jpg" class="card-img-top" alt="...">
     <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
-    </div>
+</div>
 
+
+<div class="card" style="width: 15rem;">
+    <img src="../img/carousel-theme1.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+
+<div class="card" style="width: 15rem;">
+    <img src="../img/carousel-theme1.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
 </div>
 
 </div>
@@ -40,4 +57,17 @@ export default {
 </script>
 
 <style scoped>
+.card-container {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    align-items: center;
+    height: 100%;
+    padding: 20px;
+    background-color: #f5f5f5;
+}
+
+.card-container .card {
+    margin: 10px;
+}
 </style>
